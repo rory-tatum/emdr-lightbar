bef5292cdbdf83c640692daea41f36a7405b0d22
Rory Tatum <otoshidama@gmail.com>
2026-02-15 10:26:38 -0600

    feat: remove mid_pause from bridge and web frontend

--- web/wasm_bridge.c
+++ web/wasm_bridge.c
@@ -8,13 +8,12 @@ static LightbarState state;
 static Led leds[MAX_LEDS];
 
 EMSCRIPTEN_KEEPALIVE
-void wasm_init(int num_leds, float speed, int end_pause, int mid_pause,
+void wasm_init(int num_leds, float speed, int end_pause,
                int glow_radius, int r, int g, int b) {
     if (num_leds > MAX_LEDS) num_leds = MAX_LEDS;
     config.num_leds = (uint8_t)num_leds;
     config.speed = speed;
     config.end_pause_ms = (uint16_t)end_pause;
-    config.mid_pause_ms = (uint16_t)mid_pause;
     config.glow_radius = (uint8_t)glow_radius;
     config.color.r = (uint8_t)r;
     config.color.g = (uint8_t)g;
@@ -60,11 +59,6 @@ void wasm_set_speed(float speed) {
 EMSCRIPTEN_KEEPALIVE
 void wasm_set_end_pause(int ms) {
     config.end_pause_ms = (uint16_t)ms;
-}
-
-EMSCRIPTEN_KEEPALIVE
-void wasm_set_mid_pause(int ms) {
-    config.mid_pause_ms = (uint16_t)ms;
 }
 
 EMSCRIPTEN_KEEPALIVE
